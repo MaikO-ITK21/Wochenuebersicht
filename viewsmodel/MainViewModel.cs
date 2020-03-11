629c9d0673f2a82667f6908b3d6f2010bc9113df
Maik Ohde <mohde@local>
2020-03-11 15:01:52 +0100

    feat:

--- viewsmodel/MainViewModel.cs
+++ viewsmodel/MainViewModel.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using Newtonsoft.Json;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace wochenuebersicht_2
 {
@@ -12,9 +16,14 @@ namespace wochenuebersicht_2
 
         public ObservableCollection<Wochenansicht>KW_Week{ get; set; }
 
+        public ICommand Open_KW { get; set; }
+
+        public int Selected_KW_Idx { get; set; }
+
         public MainViewModel()
         {
             KW_Week = new ObservableCollection<Wochenansicht>(Enumerable.Range(1, 52).Select(SeedKw));
+            Open_KW = new RelayCommand(Open_Day, () => true);
 
         }
 
@@ -25,6 +34,20 @@ namespace wochenuebersicht_2
                 KW = seed,
                 Woche = "Woche",
             };
+        }
+
+
+        private void Open_Day()
+        {
+
+            Wochenansicht e = KW_Week[Selected_KW_Idx];
+            string pstring = JsonConvert.SerializeObject(e);
+            Debug.WriteLine("Hallo");
+            Debug.WriteLine(pstring);
+          
+
+            DayView dayWindow = new DayView();
+            
         }
     }
 }
